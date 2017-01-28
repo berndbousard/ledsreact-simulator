236ed5b13d672e3d21f63a5d165a0c1b2be7b39d
berndbousard <bernd.bousard@student.howest.be>
2017-01-28 12:32:23 +0100

    socket.io added

---
+++ server/modules/simulator/index.js
@@ -0,0 +1,56 @@
+module.exports.register = (server, options, next) => {
+
+  const io = require(`socket.io`)(server.listener);
+
+  let directions = [];
+
+  io.on(`connection`, socket => {
+
+    const {id: socketId} = socket;
+    const {client} = socket.handshake.query;
+
+    if (client === `app`) {
+      console.log(`App is verbonden`);
+    }
+
+    if (client === `direction`) {
+      const direction = {
+        socketId,
+        batteryLevel: Math.round(Math.random() * 100)
+      };
+
+      directions.push(direction);
+
+      console.log(`Direction met Id ${direction.socketId} is verbonden`);
+
+      socket.broadcast.emit(`directionJoined`, direction);
+    }
+
+    console.log(`---`);
+    console.log(directions);
+    socket.emit(`init`, directions);
+
+    socket.on(`disconnect`, () => {
+      if (client === `direction`) {
+        console.log(`Direction met als ID ${socketId} is weg`);
+        directions = directions.filter(f => {
+          return f.socketId !== socketId;
+        });
+      }
+      console.log(`disconnect`);
+
+      if (client === `app`) {
+        console.log(`App is weg`);
+      }
+
+      socket.broadcast.emit(`updateDirections`, socketId);
+    });
+  });
+
+  next();
+};
+
+module.exports.register.attributes = {
+  name: `simulator`,
+  version: `0.1.0`
+};
--- src/js/containers/App.jsx
+++ src/js/containers/App.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import {Match, BrowserRouter as Router} from 'react-router';
+import io from 'socket.io-client';
 
 import Home from '../pages/Home';
 
-const App = () => {
-  return (
-    <Router>
-      <main>
-        <Match
-          exactly pattern='/'
-          component={Home}
-        />
-      </main>
-    </Router>
-  );
-};
+class App extends PureComponent { // Component zonder state
+
+  componentDidMount() {
+    this.socket = io(`/`, {query: `client=direction`});
+    this.socket.on(`init`);
+  }
+
+  WSInitHandler() {
+    console.log(`This Direction is Connected`);
+  }
+
+  render() {
+    return (
+      <Router>
+        <main>
+          <Match
+            exactly pattern='/'
+            component={Home}
+          />
+        </main>
+      </Router>
+    );
+  }
+}
 
 export default App;
